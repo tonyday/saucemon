52fc2f0c11877827789a6b3482b7e39018a4ba16
Tony Day <tony.day@digitaldialogue.com.au>
2012-11-16 14:10:10 +1300

    Experiment with calling git.

--- systray2.rb
+++ systray2.rb
@@ -5,29 +5,53 @@ import java.awt.event.MouseListener
 
 if java.awt.SystemTray::isSupported()
 
-  tray = java.awt.SystemTray::system_tray
-  uncheck = java.awt.Toolkit::default_toolkit.get_image("uncheck.png")
-  check = java.awt.Toolkit::default_toolkit.get_image("check.png")
+  @tray = java.awt.SystemTray::system_tray
+  @uncheck = java.awt.Toolkit::default_toolkit.get_image("uncheck.png")
+  @check = java.awt.Toolkit::default_toolkit.get_image("check.png")
 
-  popup = java.awt.PopupMenu.new
-  exit_item = java.awt.MenuItem.new("Exit")
-  exit_item.addActionListener { java.lang.System::exit(0) }
 
-  popup.add(exit_item)
-  trayIcon = TrayIcon.new(uncheck, "Tray Demo", popup)
-  trayIcon.image_auto_size = true
+  @popper = java.awt.PopupMenu.new
 
-  tray.add(trayIcon)
+  @exit_item = java.awt.MenuItem.new("Exit")
+  #check_item = java.awt.MenuItem.new("Check")
+  uncheck_item = java.awt.MenuItem.new("UnCheck")
+  @exit_item.addActionListener { java.lang.System::exit(0) }
 
-  Thread.new do
-    while true
-      if trayIcon.image == check
-        trayIcon.image = uncheck
-      else
-        trayIcon.image = check
+  @popper.add(@exit_item)
+  @trayIcon = TrayIcon.new(@uncheck, "Tray Demo", @popper)
+  @trayIcon.image_auto_size = true
+
+  @tray.add(@trayIcon)
+
+  def foo
+    @popper.removeAll
+    Dir.chdir('/Users/dduser/projects/dd-tarps') do
+      system('git fetch')
+      #%x(git log --remotes=origin --not --branches).split("\n").each do |line|
+      %x(git status).split("\n").each do |line|
+        @popper.add(java.awt.MenuItem.new(line))
       end
-      sleep 5
     end
+    @popper.add(@exit_item)
+
+    #if trayIcon.image == check
+    #  trayIcon.image = uncheck
+    #  popper.remove(check_item)
+    #  new_item = java.awt.MenuItem.new(ll.first)
+    #  popper.add(new_item)
+    #else
+    #  trayIcon.image = check
+    #  popper.remove(uncheck_item)
+    #  popper.add(check_item)
+    #end
   end
+
+  foo
+  #Thread.new do
+  #  while true
+  #    foo
+  #  end
+  #  sleep 3
+  #end
 
 end
